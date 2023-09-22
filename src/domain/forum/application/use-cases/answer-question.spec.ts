5e7c0b7497ce8b578d9379cb437a5445d7142fef
Cassiano Junior <cassianojuniorww@gmail.com>
2023-09-22 11:28:34 -0300

    feat(use-cases): add delete answer

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,13 +1,13 @@
-import { InMemoryAnswerRepository } from 'test/repositories/in-memory-answers-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
-let inMemoryAnswerRepository: InMemoryAnswerRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
 let sut: AnswerQuestionUseCase
 
 describe('Create Answer', () => {
   beforeEach(() => {
-    inMemoryAnswerRepository = new InMemoryAnswerRepository()
-    sut = new AnswerQuestionUseCase(inMemoryAnswerRepository)
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
   })
 
   it('should be able to create a answer', async () => {
@@ -18,6 +18,6 @@ describe('Create Answer', () => {
     })
 
     expect(answer.id).toBeTruthy()
-    expect(inMemoryAnswerRepository.items[0].id).toEqual(answer.id)
+    expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id)
   })
 })
